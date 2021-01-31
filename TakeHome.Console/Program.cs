dcd75b415e842f06524c2ce81d6e8f0f8a107b5b
Nick Howard <nckhwrd@gmail.com>
2021-01-31 10:40:08 -0500

    Getting the solution setup.

---
+++ TakeHome.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace GuaranteedRate.TakeHome
+{
+    class Program
+    {
+        private static IConfigurationRoot _configuration;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine($"Migration Report v{FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion}");
+            Console.WriteLine("-------------------------------------");
+
+
+        }
+    }
+}
